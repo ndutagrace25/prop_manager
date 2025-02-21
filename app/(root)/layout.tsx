abee47c8b9844641f78a046891881191f7313f89
ndutagrace25 <ndutagrace25@gmail.com>
2025-02-21 10:45:09 +0300

    added login page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import { Toaster } from "@/components/ui/toaster";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <main className="flex-1 wrapper">{children}</main>
+      <Toaster />
+    </div>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Building2 } from "lucide-react";
+
+import { useToast } from "@/hooks/use-toast";
+import { Button } from "@/components/ui/button";
+import { ToastAction } from "@/components/ui/toast";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  email: z
+    .string()
+    .min(4, {
+      message: "Email must be at least 4 characters.",
+    })
+    .email({ message: "Please enter a valid email address." }),
+  password: z.string().min(5, {
+    message: "Password must be at least 5 characters.",
+  }),
+});
+
+export default function Home() {
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    toast({
+      title: "Login success",
+      description: `${data.email} ${data.password}`,
+      action: <ToastAction altText="Goto schedule to undo">Undo</ToastAction>,
+    });
+  }
+
+  return (
+    <div className="flex justify-center items-center h-full bg-gray-50">
+      <div className="shadow-md rounded-lg py-8 flex justify-center w-[30%] bg-white">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-2/3 space-y-6 "
+          >
+            <div className="flex justify-center">
+              <Building2 className="text-blue-500" />{" "}
+              <span className="pl-2 font-bold">PropManager</span>
+            </div>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" {...field} />
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your password"
+                      {...field}
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormDescription></FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full bg-blue-600">
+              Login
+            </Button>
+            <div className="text-end text-blue-400 text-sm cursor-pointer">
+              Forgot password?
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
